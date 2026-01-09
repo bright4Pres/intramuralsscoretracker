f3f6deca860eb4f908135ba11793f2aa00dbfe03
bright <knowncarbon@gmail.com>
2026-01-09 08:17:01 +0800

    Fix SQLite deployment: add --run-syncdb and init commands

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ pip install -r requirements.txt
 
 cd scoretracker
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --run-syncdb
+python manage.py init_teams
+python manage.py init_games
 
 # Create superuser if it doesn't exist
 if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ] ; then
